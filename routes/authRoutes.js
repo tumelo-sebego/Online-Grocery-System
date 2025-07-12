986197bf891f0dc4b48ac5d8cd23ee84ed2b94cb
tumelo-sebego <145678194+tumelo-sebego@users.noreply.github.com>
2025-07-12 15:17:44 +0000

    added the verify-email route to authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser, getUserProfile } = require('../controllers/authController');
+const {
+  registerUser,
+  loginUser,
+  getUserProfile,
+  verifyEmail,
+} = require('../controllers/authController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/profile', protect, getUserProfile);
+router.get('/verify-email/:token', verifyEmail);
 
 module.exports = router;
