bc289b62f17e07fb795e6123f7d3669819b79f67
tumelo-sebego <145678194+tumelo-sebego@users.noreply.github.com>
2025-07-12 16:14:22 +0200

    Create adminModel.js

---
+++ models/adminModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const adminSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Admin = mongoose.model('Admin', adminSchema);
+module.exports = Admin;
